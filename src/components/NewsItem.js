0b290c85114758b1ce06ce105eac1670084c026c
mk07kr <mk1309kr@gmail.com>
2024-08-20 18:28:19 +0530

    Fetch API Implementataion Success

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,23 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description,imgurl,newsurl} = this.props;
+    let { title, description, imgurl, newsurl } = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imgurl} className="card-img-top" alt="..." />
+          <img
+            src={imgurl}
+            className="card-img-top"
+            alt="https://static.vecteezy.com/system/resources/previews/014/747/879/original/letter-mk-logo-with-luxury-gold-template-elegance-logo-template-vector.jpg"
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsurl} target="_blank" className="btn btn-sm btn-primary ">
+            <a
+              href={newsurl}
+              target="_blank"
+              className="btn btn-sm btn-primary "
+            >
               Explore
             </a>
           </div>
@@ -21,4 +29,3 @@ export class NewsItem extends Component {
 }
 
 export default NewsItem;
-
