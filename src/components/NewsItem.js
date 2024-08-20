c719db2fbffc8c0cdeb50b6f0b7b70181ee1e2a9
mk07kr <mk1309kr@gmail.com>
2024-08-20 17:49:41 +0530

    News App utilities added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description,imgurl } = this.props;
+    let { title, description,imgurl,newsurl} = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img src={imgurl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-sm btn-primary">
+            <a href={newsurl} target="_blank" className="btn btn-sm btn-primary ">
               Explore
             </a>
           </div>
