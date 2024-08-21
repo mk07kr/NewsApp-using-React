517d1fb594d8701e48575ba5fcb4c1064c31e727
mk07kr <mk1309kr@gmail.com>
2024-08-21 17:55:26 +0530

    Infinite scroll func added

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   constructor() {
@@ -26,53 +27,81 @@ export class News extends Component {
     });
     console.log(response);
   }
+
   async componentDidMount() {
     this.updateNews();
   }
 
-  handleNext = async () => {
-    this.setState({
-      page: this.state.page + 1,
-    });
-    this.updateNews();
-  };
+  // Buttons functions for Prev & Next
+  // handleNext = async () => {
+  //   this.setState({
+  //     page: this.state.page + 1,
+  //   });
+  //   this.updateNews();
+  // };
 
-  handlePrev = async () => {
+  // handlePrev = async () => {
+  //   this.setState({
+  //     page: this.state.page - 1,
+  //   });
+  //   this.updateNews();
+  // };
+
+  fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/everything?q=from=2024-08-19&to=2024-08-19&language=en&sortBy=popularity&apiKey=9397bf7a878c48339cff0f0fbfef76c7&page=${
+      this.state.page + 1
+    }&pageSize=10`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
     this.setState({
-      page: this.state.page - 1,
+      articles: this.state.articles.concat(parsedData.articles),
+      page: this.state.page + 1,
+      totalResults: parsedData.totalResults,
+      loading: false,
     });
-    this.updateNews();
   };
 
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h1 className="text-center">NewsApp Top headlines</h1>
-        {this.state.loading && <Spinner />}
+        {/* {this.state.loading && <Spinner />} */}
 
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title.slice(0, 40) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 85)
-                        : ""
-                    }
-                    author={element.author}
-                    imgurl={element.urlToImage}
-                    newsurl={element.url}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="container d-flex justify-content-between">
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {!this.state.loading &&
+                this.state.articles.map((element) => {
+                  return (
+                    <div className="col-md-4" key={element.url}>
+                      <NewsItem
+                        title={element.title ? element.title.slice(0, 40) : ""}
+                        description={
+                          element.description
+                            ? element.description.slice(0, 85)
+                            : ""
+                        }
+                        author={element.author}
+                        imgurl={element.urlToImage}
+                        newsurl={element.url}
+                        date={element.publishedAt}
+                        source={element.source.name}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </InfiniteScroll>
+
+        {/* Buttons for Previous and Next (Depreciated for Infinite scroll in use) */}
+
+        {/* <div className="container d-flex justify-content-between">
           <button
             type="button"
             disabled={this.state.page <= 1}
@@ -84,17 +113,25 @@ export class News extends Component {
           <button
             type="button"
             disabled={
-              this.state.page + 1 > Math.ceil(this.state.totalResults / this.state.pageSize)
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.state.pageSize)
             }
             className="btn btn-dark my-3"
             onClick={this.handleNext}
           >
             Next &rarr;
           </button>
-        </div>
-      </div>
+        </div> */}
+      </>
     );
   }
 }
 // News API Key = 9397bf7a878c48339cff0f0fbfef76c7
 export default News;
+
+// "scripts": {
+//   "start": "react-scripts start",
+//   "build": "react-scripts build",
+//   "test": "react-scripts test",
+//   "eject": "react-scripts eject"
+// },
