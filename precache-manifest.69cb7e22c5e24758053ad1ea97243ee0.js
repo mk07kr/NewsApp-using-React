dbbba7db31b60fd04213dc6568ebaaed1f6bd186
mk07kr <mk1309kr@gmail.com>
2024-08-21 19:03:07 +0530

    Updates

---
+++ precache-manifest.69cb7e22c5e24758053ad1ea97243ee0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d57fac3270d7b994c531a1943ba9ac",
+    "url": "/NewsApp-using-React/index.html"
+  },
+  {
+    "revision": "c2fe11a27d90cafea121",
+    "url": "/NewsApp-using-React/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "cba5b90d9d0404b54583",
+    "url": "/NewsApp-using-React/static/js/2.839ac876.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/NewsApp-using-React/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "c2fe11a27d90cafea121",
+    "url": "/NewsApp-using-React/static/js/main.bff660fa.chunk.js"
+  },
+  {
+    "revision": "add36fb16f855057719f",
+    "url": "/NewsApp-using-React/static/js/runtime~main.2df3993c.js"
+  },
+  {
+    "revision": "e5e9172fc240993bd699f593370b2a56",
+    "url": "/NewsApp-using-React/static/media/loading.e5e9172f.gif"
+  }
+]);
